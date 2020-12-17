2d2262419bb685fb1b7903fa59b0e243fa2f3413
Owais Islam <owaisislam@gmail.com>
2020-12-16 21:48:52 -0600

    removed unnecessary tab render code

--- src/App.js
+++ src/App.js
@@ -8,24 +8,9 @@ import Contact from "./components/Contact";
 import Resume from "./components/Resume";
 
 function App() {
-	// const [aboutSelected, setAboutSelected] = useState(true);
-	// const [portfolioSelected, setPortfolioSelected] = useState(false);
-	// const [contactSelected, setContactSelected] = useState(false);
-	// const [resumeSelected, setResumeSelected] = useState(false);
-
 	const [currentTab, setCurrentTab] = useState("about");
 
 	const renderTab = () => {
-		// if (aboutSelected) {
-		// 	return <About></About>;
-		// } else if (portfolioSelected) {
-		// 	return <Portfolio></Portfolio>;
-		// } else if (contactSelected) {
-		// 	return <Contact></Contact>;
-		// } else {
-		// 	return <Resume></Resume>;
-		// }
-
 		switch (currentTab) {
 			case "about":
 				return <About></About>;
@@ -42,19 +27,7 @@ function App() {
 
 	return (
 		<div>
-			<Header
-				// aboutSelected={aboutSelected}
-				// setAboutSelected={setAboutSelected}
-				// portfolioSelected={portfolioSelected}
-				// setPortfolioSelected={setPortfolioSelected}
-				// contactSelected={contactSelected}
-				// setContactSelected={setContactSelected}
-				// resumeSelected={resumeSelected}
-				// setResumeSelected={setResumeSelected}
-
-				currentTab={currentTab}
-				setCurrentTab={setCurrentTab}
-			></Header>
+			<Header currentTab={currentTab} setCurrentTab={setCurrentTab}></Header>
 			<main>{renderTab()}</main>
 			<Footer></Footer>
 		</div>
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,72 +1,21 @@
 import React from "react";
 
 function Navigation(props) {
-	// const {
-	// 	aboutSelected,
-	// 	setAboutSelected,
-	// 	portfolioSelected,
-	// 	setPortfolioSelected,
-	// 	contactSelected,
-	// 	setContactSelected,
-	// 	resumeSelected,
-	// 	setResumeSelected,
-	// } = props;
-
 	const { currentTab, setCurrentTab } = props;
 
 	return (
 		<nav>
 			<ul className="flex-row">
 				<li className="mx-2">
-					{/* <span
-						onClick={() => (
-							setAboutSelected(true),
-							setPortfolioSelected(false),
-							setContactSelected(false),
-							setResumeSelected(false)
-						)}
-					>
-						About Me
-					</span> */}
 					<span onClick={() => setCurrentTab("about")}>About Me</span>
 				</li>
 				<li className="mx-2">
-					{/* <span
-						onClick={() => (
-							setAboutSelected(false),
-							setPortfolioSelected(true),
-							setContactSelected(false),
-							setResumeSelected(false)
-						)}
-					>
-						Portfolio
-					</span> */}
 					<span onClick={() => setCurrentTab("portfolio")}>Portfolio</span>
 				</li>
 				<li className="mx-2">
-					{/* <span
-						onClick={() => (
-							setAboutSelected(false),
-							setPortfolioSelected(false),
-							setContactSelected(true),
-							setResumeSelected(false)
-						)}
-					>
-						Contact
-					</span> */}
 					<span onClick={() => setCurrentTab("contact")}>Contact</span>
 				</li>
 				<li className="mx-2">
-					{/* <span
-						onClick={() => (
-							() => setAboutSelected(false),
-							setPortfolioSelected(false),
-							setContactSelected(false),
-							setResumeSelected(true)
-						)}
-					>
-						Resume
-					</span> */}
 					<span onClick={() => setCurrentTab("resume")}>Resume</span>
 				</li>
 			</ul>
