c95597d7a0f8a7e412d982eb0bcf599e1d1f758d
Owais Islam <owaisislam@gmail.com>
2020-12-18 18:02:19 -0600

    updated resume page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -15,23 +15,13 @@ function About() {
 			</div>
 			<div>
 				<p>
-					Hi there! My name is Owais Islam and I hope you've come to this page
-					to see some of my work and learn some more about me. I was born in
-					Houston, TX and currently reside in Austin with my wife and our cat
-					Shuri. During the day, I work for a popular e-commerce platform
-					company. In my spare time, I love to play video games and watch comedy
-					shows. Currently we are catching up on <em>Brooklyn Nine Nine</em> and{" "}
-					<em>Veep</em>. I'm a huge Trekkie and I have seen all the episodes of
-					every single show (yes, even the animated cartoon!).
+					Hi there! My name is Owais Islam and I hope you've come to this page to see some of my work and learn some more about me. I was born in Houston, TX and currently reside in Austin with my wife and our cat Shuri. During the day, I work for a popular e-commerce platform company. In my spare time, I love to play video games and watch comedy shows. Currently we are catching up on <em>Brooklyn Nine Nine</em> and <em>Veep</em>. I'm a huge Trekkie and I have seen all the episodes of every single show (yes, even the animated cartoon!).
 				</p>
 				<p>
-					I recently picked up a copy of <em>Ghost of Tsushima</em> for the PS4
-					and it is one of the best games I have ever played.
+					I recently picked up a copy of <em>Ghost of Tsushima</em> for the PS4 and it is one of the best games I have ever played.
 				</p>
 				<p>
-					I love coding and programming and it's one of the reasons I took this
-					class. I am looking forward to expanding my knowledge and being able
-					to create great web pages.
+					I love coding and programming and it's one of the reasons I took this class. I am looking forward to expanding my knowledge and being able to create great web pages.
 				</p>
 			</div>
 		</section>
--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -3,7 +3,37 @@ import React from "react";
 function Resume() {
 	return (
 		<section>
-			<h2>Resume Section</h2>
+			<div className="center">
+				<h1 className="page-header">My Resume</h1>
+			</div>
+			<div className="bottom-spacing">
+				<a href={require("../../assets/files/Owais' Resume.pdf")} download>
+					<h4>Download my Resume</h4>
+				</a>
+			</div>
+			<div>
+				<h5>Front-End Proficiencies</h5>
+				<ol>
+					<li>HTML</li>
+					<li>CSS</li>
+					<li>JavaScript</li>
+					<li>jQuery</li>
+					<li>Bootstrap</li>
+					<li>React</li>
+					<li>responsive design</li>
+				</ol>
+				<h5>Back-End Proficiencies</h5>
+				<ol>
+					<li>APIs</li>
+					<li>Node</li>
+					<li>Express</li>
+					<li>Model View Controller (MVC)</li>
+					<li>MySQL/Sequelize/NoSQL</li>
+					<li>MongoDB/Mongoose</li>
+					<li>REST</li>
+					<li>Progressive Web Applications (PWA)</li>
+				</ol>
+			</div>
 		</section>
 	);
 }
