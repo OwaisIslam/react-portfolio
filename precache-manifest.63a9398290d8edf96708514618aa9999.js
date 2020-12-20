81d45a56dc58a42b17ea07588f3c1c44472aa73f
Owais Islam <owaisislam@gmail.com>
2020-12-19 22:39:55 -0600

    Updates

---
+++ precache-manifest.63a9398290d8edf96708514618aa9999.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7ad57d383d124c99397e961dffcf0f9",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "a1ce5133b03a4dad8734",
+    "url": "/react-portfolio/static/css/main.f1cd1086.chunk.css"
+  },
+  {
+    "revision": "75267617a7cd558b0c24",
+    "url": "/react-portfolio/static/js/2.af0d4b87.chunk.js"
+  },
+  {
+    "revision": "6bbb3a8e7360a6a98672414629b231d4",
+    "url": "/react-portfolio/static/js/2.af0d4b87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6fb096d9b70d41f103d",
+    "url": "/react-portfolio/static/js/3.c9e9ebb7.chunk.js"
+  },
+  {
+    "revision": "a1ce5133b03a4dad8734",
+    "url": "/react-portfolio/static/js/main.028ac0f6.chunk.js"
+  },
+  {
+    "revision": "a073295793d8755c9ecd",
+    "url": "/react-portfolio/static/js/runtime-main.a696ea21.js"
+  },
+  {
+    "revision": "e2056c49f51055d86ca48d1271560267",
+    "url": "/react-portfolio/static/media/Owais' Resume.e2056c49.pdf"
+  },
+  {
+    "revision": "5035699ad38fd5313bc95c4891f53581",
+    "url": "/react-portfolio/static/media/auto-tracker-image.5035699a.png"
+  },
+  {
+    "revision": "09e6ae3da5daad49c278e9ff4a75743d",
+    "url": "/react-portfolio/static/media/budget-tracker-image.09e6ae3d.png"
+  },
+  {
+    "revision": "d92efe2500ba8695d4ae6432f75b5cf9",
+    "url": "/react-portfolio/static/media/flash-beats-image.d92efe25.gif"
+  },
+  {
+    "revision": "fd14fff8beb9ae1bb3fcdf093419fe49",
+    "url": "/react-portfolio/static/media/note-taker-image.fd14fff8.png"
+  },
+  {
+    "revision": "0e54790e036d3e97597027712ffaab9b",
+    "url": "/react-portfolio/static/media/password-generator-image.0e54790e.png"
+  },
+  {
+    "revision": "111399441487363458d0a45afd105942",
+    "url": "/react-portfolio/static/media/recent-photo.11139944.jpg"
+  },
+  {
+    "revision": "5db07b5ca4b360c2f9058145e3070ff4",
+    "url": "/react-portfolio/static/media/weather-dashboard-image.5db07b5c.png"
+  },
+  {
+    "revision": "0d20b9eb766c0d0e2b565e2a3ef764d2",
+    "url": "/react-portfolio/static/media/webpage-screenshot.0d20b9eb.png"
+  }
+]);
