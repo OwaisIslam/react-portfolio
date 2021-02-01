0c4b4addc8955833d190739dc28b3c0ca18eaa33
Owais Islam <owaisislam@gmail.com>
2021-01-31 20:46:49 -0600

    updated projects with Living Real

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,74 +1,91 @@
-import React from "react";
-import Project from "../Project";
+import React from 'react';
+import Project from '../Project';
 
 function Portfolio() {
 	const projects = [
 		{
-			name: "Auto Tracker",
+			name: 'Living Real',
 			description:
-				"This is your personal automobile information tracking system. Track time dependent, vehicle specific information from a single application, available on any web-enabled device.",
-			image: "auto-tracker-image.png",
+				'This is a property management system that is used by landlords viewing and managing their properties and for tennants to allow for maintenance requests and pay rent..',
+			image: 'living-real-image.png',
 			technologies: [
-				"HTML/CSS",
-				"JavaScript",
-				"Node.js",
-				"Handlebars.js",
-				"Express & Sequelize",
-				"Heroku & AWS S3",
+				'HTML/CSS',
+				'JavaScript',
+				'Node.js',
+				'Express & NoSQL',
+				'Heroku & AWS S3',
+				'React.js',
+				'MERN Stack',
 			],
-			github: "https://github.com/p2T3/auto-tracker",
-			deployed: "https://team-3-auto-tracker.herokuapp.com/",
+			github: 'https://github.com/P3T2-Realty-Rogues/living-real',
+			deployed: 'https://living-real.herokuapp.com/',
 		},
 		{
-			name: "Flash Beats",
+			name: 'Auto Tracker',
 			description:
-				"A clean and accurate way to search for your favorite songs while also displaying the lyrics.",
-			image: "flash-beats-image.gif",
-			technologies: ["HTML/CSS", "JavaScript", "jQuery"],
-			github: "https://github.com/TeamFlash1/music-app",
-			deployed: "https://teamflash1.github.io/music-app/",
-		},
-		{
-			name: "Budget Tracker",
-			description:
-				"Budget Tracker is an Application that uses a noSQL database and offline persistance for a great user experience for managing their finances and budgeting no matter where they are.",
-			image: "budget-tracker-image.png",
+				'This is your personal automobile information tracking system. Track time dependent, vehicle specific information from a single application, available on any web-enabled device.',
+			image: 'auto-tracker-image.png',
 			technologies: [
-				"JavaScript",
-				"MongoDB",
-				"IndexedDB & Service Workers",
-				"Node.js",
-				"Express",
+				'HTML/CSS',
+				'JavaScript',
+				'Node.js',
+				'Handlebars.js',
+				'Express & Sequelize',
+				'Heroku & AWS S3',
 			],
-			github: "https://github.com/OwaisIslam/budget-tracker",
-			deployed: "https://budget-tracker-owais.herokuapp.com/",
+			github: 'https://github.com/OwaisIslam/auto-tracker',
+			deployed: 'https://team-3-auto-tracker.herokuapp.com/',
 		},
 		{
-			name: "Weather Dashboard",
+			name: 'Flash Beats',
 			description:
-				"A weather dashboard that shows current weather and five-day weather forecast for any city.",
-			image: "weather-dashboard-image.png",
-			technologies: ["HTML/CSS", "JavaScript"],
-			github: "https://github.com/OwaisIslam/weather-dashboard",
-			deployed: "https://owaisislam.github.io/weather-dashboard/",
+				'A clean and accurate way to search for your favorite songs while also displaying the lyrics.',
+			image: 'flash-beats-image.gif',
+			technologies: ['HTML/CSS', 'JavaScript', 'jQuery'],
+			github: 'https://github.com/OwaisIslam/flash-beats',
+			deployed: 'https://teamflash1.github.io/music-app/',
 		},
 		{
-			name: "Password Generator",
+			name: 'Budget Tracker',
 			description:
-				"This is an application that allows you to generate a password from 8 to 128 characters based on criteria you select.",
-			image: "password-generator-image.png",
-			technologies: ["HTML/CSS", "JavaScript"],
-			github: "https://github.com/OwaisIslam/password-generator",
-			deployed: "https://owaisislam.github.io/password-generator/",
+				'Budget Tracker is an Application that uses a noSQL database and offline persistance for a great user experience for managing their finances and budgeting no matter where they are.',
+			image: 'budget-tracker-image.png',
+			technologies: [
+				'JavaScript',
+				'MongoDB',
+				'IndexedDB & Service Workers',
+				'Node.js',
+				'Express',
+			],
+			github: 'https://github.com/OwaisIslam/budget-tracker',
+			deployed: 'https://budget-tracker-owais.herokuapp.com/',
 		},
 		{
-			name: "Note Taker",
+			name: 'Weather Dashboard',
 			description:
-				"This application allows the user to enter notes and as they save, they appear in the sidebar on the left. Notes can be deleted by clicking the trash can next to the corresponding note.",
-			image: "note-taker-image.png",
-			technologies: ["HTML/CSS", "JavaScript", "Express.js"],
-			github: "https://github.com/OwaisIslam/note-taker",
-			deployed: "https://note-taker-owais.herokuapp.com/",
+				'A weather dashboard that shows current weather and five-day weather forecast for any city.',
+			image: 'weather-dashboard-image.png',
+			technologies: ['HTML/CSS', 'JavaScript'],
+			github: 'https://github.com/OwaisIslam/weather-dashboard',
+			deployed: 'https://owaisislam.github.io/weather-dashboard/',
+		},
+		{
+			name: 'Password Generator',
+			description:
+				'This is an application that allows you to generate a password from 8 to 128 characters based on criteria you select.',
+			image: 'password-generator-image.png',
+			technologies: ['HTML/CSS', 'JavaScript'],
+			github: 'https://github.com/OwaisIslam/password-generator',
+			deployed: 'https://owaisislam.github.io/password-generator/',
+		},
+		{
+			name: 'Note Taker',
+			description:
+				'This application allows the user to enter notes and as they save, they appear in the sidebar on the left. Notes can be deleted by clicking the trash can next to the corresponding note.',
+			image: 'note-taker-image.png',
+			technologies: ['HTML/CSS', 'JavaScript', 'Express.js'],
+			github: 'https://github.com/OwaisIslam/note-taker',
+			deployed: 'https://note-taker-owais.herokuapp.com/',
 		},
 	];
 
