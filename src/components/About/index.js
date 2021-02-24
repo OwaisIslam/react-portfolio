ad96800681bbcb0b6f6696b2dba1e063a791fd36
Owais Islam <owaisislam@gmail.com>
2021-02-24 15:23:23 -0600

    fixed spacing and screenshot

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -28,9 +28,20 @@ function About() {
 				</p>
 				<p>
 					I applied aspects of UX and agile development in a
-					recent project. I worked on a team of five to
-					<a href="https://github.com/P3T2-Realty-Rogues/living-real" target="_blank"> develop</a>
-					a <a href="https://living-real.herokuapp.com/" target="_blank"> single-page MERN app </a>
+					recent project. I worked on a team of five to{' '}
+					<a
+						href="https://github.com/P3T2-Realty-Rogues/living-real"
+						target="_blank"
+					>
+						develop
+					</a>{' '}
+					a{' '}
+					<a
+						href="https://living-real.herokuapp.com/"
+						target="_blank"
+					>
+						single-page MERN app
+					</a>{' '}
 					that helps property owners advertise their properties,
 					manage their tenants and manage their real estate all
 					in one place. I’m excited to leverage my skills as part
